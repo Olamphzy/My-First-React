9b5612e45ce3b66c1d16c97cffc6eedba733685d
Olamphzy <61218900+Olamphzy@users.noreply.github.com>
2022-10-28 15:42:53 +0100

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="ui fixed menu">
+            <div className="ui container center">
+                <h1>Contact Manager (Olamphzy)</h1>
+            </div>
+        </div>
+    );
+};
+export default Header;
